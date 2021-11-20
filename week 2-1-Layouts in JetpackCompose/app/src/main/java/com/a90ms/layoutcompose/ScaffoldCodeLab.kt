b9e2317af10299ef5986a2f6c14e62aed9088c86
90MS <myungsung510@gamil.com>
2021-11-20 14:10:38 +0900

    scaffold

---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/a90ms/layoutcompose/ScaffoldCodeLab.kt
@@ -0,0 +1,49 @@
+package com.a90ms.layoutcompose
+
+import android.widget.ImageButton
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.a90ms.layoutcompose.ui.theme.LayoutsCodelabTheme
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
+@Preview
+@Composable
+fun ScaffoldPreview() {
+    LayoutsCodelabTheme {
+        LayoutsCodelab()
+    }
+}
